d155cf9271af5e5f91fadcfb4e8e3752def66b2b
JoppeBijlsma <68710324+TheJoppeBijlsma@users.noreply.github.com>
2024-02-28 00:38:32 +0100

    added first part of new header

---
+++ development/Scripts/Header-Color-Change.js
@@ -0,0 +1,15 @@
+// JavaScript Document
+$(document).ready(function(){
+  $(window).scroll(function(){
+   var scroll = $(window).scrollTop();
+	if (scroll > 10) {
+		$(".header").css("background" , "#060714");
+		$(".header").css("box-shadow" , "0px 26px 30px -10px rgba(0, 0, 0, 0.69), 0px 16px 10px -10px rgba(0, 0, 0, 0.73)");
+	}
+
+	else{
+		$(".header").css("background" , "rgba(0,0,0,0.00)");
+		$(".header").css("box-shadow" , "0px 26px 30px -10px rgba(0, 0, 0, 0.0), 0px 16px 10px -10px rgba(0, 0, 0, 0.0)");
+	}
+  })
+})
