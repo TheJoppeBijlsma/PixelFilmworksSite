b7515ca4579ae4e4e86eec985f715af9b22ddd29
JoppeBijlsma <68710324+TheJoppeBijlsma@users.noreply.github.com>
2024-03-02 01:53:38 +0100

    reduced dropshadow

--- development/Scripts/Header-Color-Change.js
+++ development/Scripts/Header-Color-Change.js
@@ -4,12 +4,12 @@ $(document).ready(function(){
    var scroll = $(window).scrollTop();
 	if (scroll > 10) {
 		$(".header").css("background" , "#060714");
-		$(".header").css("box-shadow" , "0px 26px 30px -10px rgba(0, 0, 0, 0.69), 0px 16px 10px -10px rgba(0, 0, 0, 0.73)");
+		$(".header").css("box-shadow" , "0px 26px 30px -10px rgba(0, 0, 0, 0.69)");
 	}
 
 	else{
 		$(".header").css("background" , "rgba(0,0,0,0.00)");
-		$(".header").css("box-shadow" , "0px 26px 30px -10px rgba(0, 0, 0, 0.0), 0px 16px 10px -10px rgba(0, 0, 0, 0.0)");
+		$(".header").css("box-shadow" , "0px 26px 30px -10px rgba(0, 0, 0, 0.0)");
 	}
   })
 })
