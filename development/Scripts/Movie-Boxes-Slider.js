405912c6a6784bed76c8dd7c5e7f399bf2198a6f
JoppeBijlsma <68710324+TheJoppeBijlsma@users.noreply.github.com>
2024-03-04 18:31:32 +0100

    dynamisch maken pagina

--- development/Scripts/Movie-Boxes-Slider.js
+++ development/Scripts/Movie-Boxes-Slider.js
@@ -12,33 +12,12 @@ $(document).ready(function(){
 		centerMode: false,
 		responsive: [
 			{
-				breakpoint: 1450,
-				settings: {
-					slidesToScroll: 4,
-					slidesToShow: 4
-				}
-			},
-			{
-				breakpoint: 1200,
-				settings: {
-					slidesToScroll: 3,
-					slidesToShow: 3
-				}
-			},
-			{
-				breakpoint: 900,
+				breakpoint: 741,
 				settings: {
 					slidesToScroll: 2,
 					slidesToShow: 2
 				}
-			},
-			{
-				breakpoint: 600,
-				settings: {
-					slidesToScroll: 1,
-					slidesToShow: 1
-				}
-			},
+			}
 		]
   });
 });  
